0dc75aafb4f2ded572d283a6851360d1a21424fe
babasingh123 <firexavinash1@gmail.com>
2025-01-08 19:56:00 +0530

    Initial commit

---
+++ src/main/java/com/toppickhub/backend/ToppickhubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.toppickhub.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToppickhubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToppickhubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toppickhub/backend/ToppickhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toppickhub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToppickhubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
